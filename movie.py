d32b97fc431b200cc537121d651c42ebefc8063d
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-29 08:35:01 -0500

    Updated from Brython Server: 1/29/2018 8:35:00 AM

--- movie.py
+++ movie.py
@@ -11,5 +11,5 @@ elif 13<=age<17:
     print("Watch PG-13 movies! The MPAA says you can watch this category when you are younger as long as you exercize caution. The MPAA also says you can watch R rated movies with parental guidance")
     
 else:
-    print("YOu can now watch NC-17 movies and R rated movies alone in theaters.")
+    print("YOu can now watch NC-17 movies and R rated movies alone in theaters. Congrats?")
     
