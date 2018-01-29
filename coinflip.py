c4a53053216cb66dfb7080ada122886f3090fc18
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-29 11:33:00 -0500

    Updated from Brython Server: 1/29/2018 11:32:59 AM

--- coinflip.py
+++ coinflip.py
@@ -3,11 +3,16 @@
 #coinFlip.py flips a coun
 
 from random import randint
+
 flip=input("Type 'flip' to flip the coin  ")
+
 rand=randint(1,2)
+
 if flip=="flip":
+    
     if rand==1:
         print("Heads")
+        
     elif rand==2:
         print("Tails")
         
