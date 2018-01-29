d766bc1b4edbe06508416454cc9057b285eb238e
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-29 10:39:45 -0500

    Updated from Brython Server: 1/29/2018 10:39:45 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,33 @@
+#Daniel
+#1/29/18
+#fortuneTeller.py displays fortune
+print("Take these colors: red and blue"
 
+col=int(input("input the color of your choice"))
+
+num=int(input("Pick a number between 1-4")
+
+if color=="red" and num==1:
+    print("You will get hit by an 18 wheeler and die")
+    
+elif color=="red" and num==2:
+    print("You will be immortal")
+    
+elif color=="red" and num==3:
+    print("You choke on a fish bone")
+
+elif color=="red" and num==4:
+    print("You die in prison")
+    
+elif color=="blue" and num==1:
+    print("You die after you eat tainted meat")
+    
+elif color=="blue" and num==2:
+    print("Obesity claims you")
+    
+elif color=="blue and num==3:
+    print("Cancer. Sorry. You have it")
+
+else:
+    print("You disappear)
+    
