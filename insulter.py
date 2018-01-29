424ad3b0e184bc777402440f3f87d324381f4ba7
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-29 11:10:24 -0500

    Updated from Brython Server: 1/29/2018 11:10:22 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,30 @@
+#Daniel
+#1/29/18
+#insulter.py slaps people with a harsh insult
+
+from_random_import_randint
+
+name=input("What is your name?")
+
+random=randint(1,5)
+
+if random==1:
+    print(name, ", your socks smell!")
+    
+elif random==2:
+    print(name, ", your face!")
+    
+elif random==3:
+    print(name, ", why are you even here?")
+
+elif random==4:
+    print(name, ", you remind me 
+
+elif random==5:
+    print(name, ", you look like a dog!")
+    
+else:
+    print("You are so stupid you broke my code")
+    
+
 
