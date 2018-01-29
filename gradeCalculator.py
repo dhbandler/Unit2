d990bf6f921ef0d5fd0114e953f32a89d9f20b4f
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-29 08:26:32 -0500

    Updated from Brython Server: 1/29/2018 8:26:31 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,22 @@
+#Daniel
+#1/29/18
+#gradeCalculator.py calculates grade from percent
+
+perc = float(input("Input what you got here  "))
+
+if perc<60:
+    print("You got an F. Boooooooooo. You should feel bad.")
+    
+elif 60<=perc<70:
+    print("You got a D. Did you even study?, or are you stupid?")
+    
+elif 70<=perc<80:
+    print("You got a C. Not impressive.")
+    
+elif 80<=perc<90:
+    print("You got a B. Not bad, but I've seen better")
+    
+else:
+    print("Congrats. You got an A. NOW GO BACK TO STUDYING YOU LAZY INGRATE!")
+    
 
