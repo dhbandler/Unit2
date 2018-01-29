7cfdff370ffbe7160fe4a1b60bc1b30dc63a9f28
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-01-29 08:18:31 -0500

    Updated from Brython Server: 1/29/2018 8:18:31 AM

--- ifDemo.py
+++ ifDemo.py
@@ -1,1 +1,12 @@
+#Daniel 
+#1/29/18
+#ifdemo.py if, elif, else statements
 
+num=float(input("Input your fave # here")
+
+if num>0:
+    print(num, "Is postive")
+elif num<0:
+    print(num, "is negative")
+else:
+    print(num, "is zero really)")
