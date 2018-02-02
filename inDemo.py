8c8e9732d08689033e95c2b738bbdae96c37c066
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-02 08:31:09 -0500

    Updated from Brython Server: 2/2/2018 8:31:06 AM

--- inDemo.py
+++ inDemo.py
@@ -1,1 +1,10 @@
+#Daniel
+#1/2/18
+#inDemo.py searches string for specific character
 
+word=input("Input word here   ")
+
+if "w" or "W" in word:
+    print(word, "is cool")
+else:
+    print(word, "is not cool")
