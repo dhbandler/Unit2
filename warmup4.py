7e7d165d607cd652dbdd065103380bbeda800daa
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-02 08:28:11 -0500

    Updated from Brython Server: 2/2/2018 8:28:10 AM

--- warmup4.py
+++ warmup4.py
@@ -1,1 +1,11 @@
+#Daniel Bandler
+#1/2/118
+#warmup4.py Ask user for a number, print "buzz" if it has a 7 in in or is div by 7, otherwise pring #
 
+num=int(input("Input a number "))
+
+if "7" in str(num) or num%7 == 0:
+    print("Buzz")
+else:
+    print(num)
+    
