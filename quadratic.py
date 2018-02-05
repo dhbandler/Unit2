5294c5b0821c44f303f03914e0918cb968d60dec
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-05 08:49:06 -0500

    Updated from Brython Server: 2/5/2018 8:49:06 AM

--- quadratic.py
+++ quadratic.py
@@ -1,1 +1,9 @@
+#Daniel
+#2/5/18
+#quadratic.py findz zeros of quadratic
 
+
+
+a=float(input("Input the first coefficient. "))
+b=float(input("Input the second coefficient. "))
+c=float(input("Input the third coefficient. "))
