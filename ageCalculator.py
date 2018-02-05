48ba43b4368c60007e0e1b44f0b67dc1a535c5ad
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-05 08:44:06 -0500

    Updated from Brython Server: 2/5/2018 8:44:06 AM

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,1 +1,25 @@
+#Daniel Bandler
+#1/5/18
+#ageCalculator.py Calculates one's age
 
+from datetime import date
+
+year=int(input("What year were you born in? "))
+
+
+
+month=int(input("What month were you born in? "))
+
+
+
+day=int(input("What day of the month were you born on? "))
+
+
+tentage=date.today().year-year
+
+if month < date.today().month:
+    print(tentage)
+elif month == date.today().month and day <= date.today().day:
+    print(tentage)
+else:
+    print(tentage-1)
