f08749046e9e17990d00fcc0352cd4c385c37347
dhbandler <35492823+dhbandler@users.noreply.github.com>
2018-02-15 08:49:15 -0500

    Updated from Brython Server: 2/15/2018 8:49:12 AM

--- unitConverter.py
+++ unitConverter.py
@@ -1,28 +1,37 @@
 #Daniel
 #1/19/18
 #unitConverter.py converts units
-print("Do you want to convert:")
-print("1. Kilometers to Miles")
-print("2. Kilograms to Pounds")
-print("3. Liters to Gallons")
-print("4. Celsius to Fahrenheit")
-choi=int(input("Type the number that corresponds with the type of conversion you want to do "))
+i = 0
+while i == 0:
+    print("Do you want to convert:")
+    print("1. Kilometers to Miles")
+    print("2. Kilograms to Pounds")
+    print("3. Liters to Gallons")
+    print("4. Celsius to Fahrenheit")
+    print("5. Type '5' or 'quit' to quit")
 
-if choi == 1:
-    km=float(input("Input how many km you want to convert "))
-    print(km*0.621371, "miles")
+    choi=int(input("Type the number that corresponds with the type of conversion you want to do "))
+
+    if choi == 1:
+        km=float(input("Input how many km you want to convert "))
+        print(km*0.621371, "miles")
     
-elif choi == 2: 
-    kg=float(input("Input how many kg you want to convert "))
-    print(kg*2.20462, "lbs")
+    elif choi == 2: 
+        kg=float(input("Input how many kg you want to convert "))
+        print(kg*2.20462, "lbs")
 
-elif choi == 3:
-    li=float(input("Input how many litres you want to convert "))
-    print(li*0.264172, "gallons")
+    elif choi == 3:
+        li=float(input("Input how many litres you want to convert "))
+        print(li*0.264172, "gallons")
     
-elif choi == 4:
-    cel=float(input("Input how many degrees in Celsius you want to convert "))
-    print(cel*(9/5)+32, "degrees F")
+    elif choi == 4:
+        cel=float(input("Input how many degrees in Celsius you want to convert "))
+        print(cel*(9/5)+32, "degrees F")
+    
+    elif choi == "quit" or choi == 5:
+        print("Alright, I'll quit")
+        break
 
-else:
-    print(" How hard is it to type an ineger between 1 and 4 you silly piece of Broccoli?")
+    else:
+        print(" Error. How hard is it to type an ineger between 1 and 4 you silly piece of Broccoli?")
+
